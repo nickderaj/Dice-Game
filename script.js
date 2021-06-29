3a4813b1ec9d2e1d375b156cac25d18d3ffc6d4c
Nick De Raj <nickderaj@gmail.com>
2021-06-29 13:54:15 +0800

    Adding a Rules button and functionality

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-"use strict";
+'use strict';
 
 // Selecting elements
-const player0El = document.querySelector(".player--0");
-const player1El = document.querySelector(".player--1");
-const score0El = document.querySelector("#score--0");
-const score1El = document.getElementById("score--1");
-const current0El = document.querySelector("#current--0");
-const current1El = document.getElementById("current--1");
-const diceEl = document.querySelector(".dice");
-const btnNew = document.querySelector(".btn--new");
-const btnRoll = document.querySelector(".btn--roll");
-const btnHold = document.querySelector(".btn--hold");
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.querySelector('#current--0');
+const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 let scores, currentScore, activePlayer, playing;
 
@@ -21,12 +21,12 @@ const init = function () {
   playing = true;
   document.getElementById(`current--${activePlayer}`).textContent =
     currentScore;
-  player0El.classList.add("player--active");
-  player1El.classList.remove("player--active");
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
   document.querySelector(`#score--0`).textContent = scores[0];
   document.querySelector(`#score--1`).textContent = scores[1];
-  document.querySelector(`.player--0`).classList.remove("player--winner");
-  document.querySelector(`.player--1`).classList.remove("player--winner");
+  document.querySelector(`.player--0`).classList.remove('player--winner');
+  document.querySelector(`.player--1`).classList.remove('player--winner');
 };
 
 init();
@@ -36,20 +36,20 @@ const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent =
     currentScore;
   activePlayer = activePlayer === 0 ? 1 : 0;
-  player0El.classList.toggle("player--active");
-  player1El.classList.toggle("player--active");
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
 };
 
 // Starting elements
 score0El.textContent = 0;
 score1El.textContent = 0;
-diceEl.classList.add("hidden");
+diceEl.classList.add('hidden');
 
-btnRoll.addEventListener("click", function () {
+btnRoll.addEventListener('click', function () {
   if (playing) {
     const dice = Math.trunc(Math.random() * 6) + 1;
-    diceEl.classList.remove("hidden");
-    diceEl.src = `dice-${dice}.png`;
+    diceEl.classList.remove('hidden');
+    diceEl.src = `img/dice-${dice}.png`;
     if (dice !== 1) {
       // Add dice to current score
       currentScore += dice;
@@ -62,24 +62,52 @@ btnRoll.addEventListener("click", function () {
   }
 });
 
-btnHold.addEventListener("click", function () {
+btnHold.addEventListener('click', function () {
   if (playing) {
     scores[activePlayer] += currentScore;
     document.querySelector(`#score--${activePlayer}`).textContent =
       scores[activePlayer];
     if (scores[activePlayer] >= 100) {
       playing = false;
-      diceEl.classList.add("hidden");
+      diceEl.classList.add('hidden');
       document
         .querySelector(`.player--${activePlayer}`)
-        .classList.add("player--winner");
+        .classList.add('player--winner');
       document
         .querySelector(`.player--${activePlayer}`)
-        .classList.remove("player--active");
+        .classList.remove('player--active');
     } else {
       switchPlayer();
     }
   }
 });
 
-btnNew.addEventListener("click", init());
+btnNew.addEventListener('click', init);
+
+// Rules:
+const rules = document.querySelector('.menu');
+const overlay = document.querySelector('.overlay');
+const btnCloseRules = document.querySelector('.close-menu');
+const btnsOpenRules = document.querySelector('.btn--rules');
+
+const openRules = function () {
+  rules.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeRules = function () {
+  rules.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+
+btnsOpenRules.addEventListener('click', openRules);
+btnCloseRules.addEventListener('click', closeRules);
+overlay.addEventListener('click', closeRules);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !rules.classList.contains('hidden')) {
+    closeRules();
+  }
+});
+
